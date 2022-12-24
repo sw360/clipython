6c43b69b0cd831defdb71bdf484c0850bcb08f4a
Thomas Graf <tngraf@gmx.de>
2022-12-24 11:28:57 +0100

    chores: remove Siemens specific stuff

--- Upload_to_BT_Artifactory.ps1
+++
@@ -1,7 +0,0 @@
-# --------------------------------------------
-# Upload Python Packages 
-# --------------------------------------------
-
-# 2019-08-27, T. Graf
-
-twine upload --verbose --repository-url https://devops.bt.siemens.com/artifactory/api/pypi/pypi-siemens dist\* -u $env:artifactory_user -p $env:artifactory_password
