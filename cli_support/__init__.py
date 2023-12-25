cfa7a2633d5987eb940912e76dad5c7de6d73d48
Thomas Graf <tngraf@gmx.de>
2023-12-25 11:30:37 +0100

    feat: release version 2.0.0

--- cli_support/__init__.py
+++ cli_support/__init__.py
@@ -7,7 +7,7 @@
 # SPDX-License-Identifier: MIT
 # -------------------------------------------------------------------------------
 
-__version__ = (2, 0)
+__version__ = (2, 0, 0)
 
 # isort: skip_file
 from .cli_copyright import CliCopyright
