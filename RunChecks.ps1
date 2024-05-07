d08ce0ea0559f1cee1f2eb2452a241c92bc6da06
Thomas Graf <thomas.graf@siemens.com>
2024-05-07 17:34:19 +0200

    chores: be more verbose while running checks

--- RunChecks.ps1
+++ RunChecks.ps1
@@ -2,13 +2,21 @@
 # Run quality checks
 # ------------------
 
-# 2023-12-24, T. Graf
+# 2024-05-07, T. Graf
 
+Write-Host "flake8 ..."
 poetry run flake8
+
+Write-Host "markdownlint ..."
 npx -q markdownlint-cli *.md
+
+Write-Host "isort ..."
 poetry run isort .
+
+Write-Host "mypy ..."
 poetry run mypy .
 
+Write-Host "Done."
 
 # -----------------------------------
 # -----------------------------------
